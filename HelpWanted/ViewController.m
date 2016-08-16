2c88fc2698eb07f360ec7a11666f8d0e47bc9cd2
Miquel <miquel@mac-pro-de-miquel.local>
2016-08-16 06:13:25 +0200

    Commit github

--- HelpWanted/ViewController.m
+++ HelpWanted/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     // Comentario
+    // Comentario2
 }
 
 - (void)didReceiveMemoryWarning {

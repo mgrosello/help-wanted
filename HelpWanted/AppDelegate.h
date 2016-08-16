e6b8d51be7263a3a7e8e278f3e55ef05399a1c97
Miquel <miquel@mac-pro-de-miquel.local>
2016-08-16 06:07:16 +0200

    Initial Commit

---
+++ HelpWanted/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelpWanted
+//
+//  Created by Miquel on 16/8/16.
+//  Copyright © 2016 Miquel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelpWanted/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelpWanted
+//
+//  Created by Miquel on 16/8/16.
+//  Copyright © 2016 Miquel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
